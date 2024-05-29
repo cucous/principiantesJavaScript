e3bcd53875038c6cbbdda1edbd9f1c78e6dcefeb
cucou <iva-cucou@hotmail.com>
2024-05-29 08:43:29 -0600

    feat: Update script source for arithmetic operators

---
+++ 02-operadores/06-bitwise.js
@@ -0,0 +1,43 @@
+// Decimal: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+// Binario: 0, 1
+// Bit: 0 o 1
+// Byte: 8 bits
+// Byte: 00000000 -> 0
+// Byte: 00000001 -> 1
+// Byte: 00000010 -> 1
+// Byte: 00000011 -> 3
+// Byte: 00000100 -> 4
+// Byte: 00000101 -> 5
+// Byte: 00000110 -> 6
+// Byte: 00000111 -> 7
+// Byte: 00001000 -> 8
+
+// Operadores bitwise
+// & -> and
+// | -> or
+// ~ -> not
+// ^ -> xor
+// << -> desplazamiento a la izquierda
+// >> -> desplazamiento a la derecha
+// >>> -> desplazamiento a la derecha sin signo
+
+console.log(1 | 3); // 00000011 -> 3
+console.log(1 | 4); // 00000101 -> 5
+console.log(3 | 5); // 00000111 -> 7
+
+
+console.log(1 & 3); // 00000001 -> 1
+console.log(1 & 4); // 00000000 -> 0
+console.log(3 & 5); // 00000001 -> 1
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 02-operadores/08-ternario.js
@@ -0,0 +1,5 @@
+// expresion ? si es true : si es false
+const edad = 18;
+const acceso = edad > 17 ? 'Acceso permitido' : 'Acceso  denegado';
+
+console.log(acceso); 
