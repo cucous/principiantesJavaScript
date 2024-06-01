2918f11d742b0a602f810bc9a5cfe08cddbab1eb
cucou <iva-cucou@hotmail.com>
2024-06-01 14:38:22 -0600

    feat: Update script source to include new JavaScript file

---
+++ 05-objetos/01-intro.js
@@ -0,0 +1,23 @@
+/*
+const email = 'iva-cucou1@gmail.com';
+const name = 'Ivan';
+const direccion = {
+    street: 'Calle 123',
+    number: 123
+};
+*/
+// ⬇️⬇️⬇️ Programación orientada a objetos ⬇️⬇️⬇️
+
+// Crear un objeto que represente un usuario
+let user = {
+    email: 'iva-cucou1@gmail.com',
+    name: 'Ivan',
+    direccion: {
+        street: 'Calle 123',
+        number: 123
+    },
+    activo: true,
+    recuperarPassword: function () {// Funcsión anónima
+        console.log('Recuperando password...');
+    }
+};
